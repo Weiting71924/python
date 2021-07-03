e0972304802bee9d0df30999aca5dbd4088cade2
jack chen <86872075+Weiting71924@users.noreply.github.com>
2021-07-03 09:35:39 +0000

    print

---
+++ 20210703/class/0703.py
@@ -0,0 +1,17 @@
+print(1+2+3+4+5)
+print(1+2*3/4)
+print('hellow world')
+pi=3.14
+print(pi)
+print(1+2+3+4+5)
+print(1+2*3/4)
+print('hellow world')
+pi=3.14
+print(pi)
+print(1+2+3+4+5)
+print(1+2*3/4)
+print('hellow world')
+pi=3.14
+print('asg'*5)
+a54456='g ;kmef'
+print(a55456*54)
