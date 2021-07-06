a29cc0c8020f87592011798d9789b4ad40e436c1
jack chen24 <86872075+Weiting71924@users.noreply.github.com>
2021-07-06 12:36:08 +0000

    python

---
+++ 20210706/0706 3.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+
+image = img.imread("The-letter-the-alphabet-22187590-2560-2560.jpg")
+plt.imshow(image)
+plt.show()
---
+++ main.py
@@ -0,0 +1,3 @@
+a=int(input("輸入r:"))
+pi=3.14
+print(pi*a**2)
