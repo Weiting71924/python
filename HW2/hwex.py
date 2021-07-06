395bbe38eacaeec1b0611acbd2e6dce17f151684
jack chen24 <86872075+Weiting71924@users.noreply.github.com>
2021-07-06 14:21:06 +0000

    python

---
+++ HW2/hwex.py
@@ -0,0 +1,20 @@
+"""
+Topic:
+
+Show:please input how many steak do you sell:
+Input:90
+Output:
+We sell 90 steak
+We sell 75 pork
+
+"""
+
+"""
+Topic:華氏溫度轉攝氏溫度
+
+Show:Please input Celsius Temperature:
+Input:60
+Output:
+60.0F = 15.6C
+
+"""
