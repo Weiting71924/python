dd64b31d40733be1edcd026b29e0a16b51890950
jack chen24 <86872075+Weiting71924@users.noreply.github.com>
2021-07-10 09:39:39 +0000

    python

---
+++ main.py
@@ -0,0 +1,38 @@
+# pi=3.14
+# del pi
+# print(pi)
+
+# x=4
+# x+=3
+# print(x)
+# m=float(input("輸入身高:"))
+# g=int(input("\n輸入體重:"))
+# bmi=g/(m**2)
+# print("\nBMI={0:.2f}".format(bmi))
+# print(1==1)
+# print(1==1.0)
+# print(1=="1")
+# print(1 != 0)
+# print(1 != 1.0)
+# print(1 != "1")
+# print((1.7 != (2.7 - 1.0)))
+# print(2.7-1.0)
+# word = input("請輸入密碼:")
+# if word == "6666":
+#     print("歡迎光臨")
+# elif:
+# else:
+#     print("錯誤")
+# print("程式結束")
+
+a = int(input("請輸入成績:"))
+if a >= 90:
+    print("優")
+elif a >= 80:
+    print("甲")
+elif a >= 70:
+    print("乙")
+elif a >= 60:
+    print("丙")
+else:
+    print("丁")
