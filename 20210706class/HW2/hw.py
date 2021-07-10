c85f49d830af59cbc28547ea3beb01dcbe26c46a
jack chen24 <86872075+Weiting71924@users.noreply.github.com>
2021-07-10 08:10:02 +0000

    python

---
+++ 20210706class/HW2/hw.py
@@ -0,0 +1,8 @@
+a = int(input("How many steak do you sell:"))
+str(print("\nWe sell  " + a + "  steak\n"))
+ans = str(a * 5 / 6)
+print("We sell " + ans + " pork")
+
+b = int(input("Please input Celsius Temperature:"))
+answer = str((b - 32) * 5 / 9)
+print("\n=" + answer + "℃")
