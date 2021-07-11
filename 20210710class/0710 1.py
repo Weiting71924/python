2c64c1322a4f78dfd317af968ec526f08cd4cc99
jack chen24 <86872075+Weiting71924@users.noreply.github.com>
2021-07-11 08:25:26 +0000

    0711

--- 20210710class/0710 1.py
+++ 20210710class/0710 1.py
@@ -3,5 +3,5 @@ del pi
 print(pi)
 
 x=4
-x+=3 # = x=x+3
+x+=3 #= x=x+3
 print(x)
--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-"""
-Topic:輸入溫度，如果溫度>=40度C,顯示: 太熱, 
-　　　如果溫度<= 10 顯示:太冷, 其他：舒適:
-​
-Show:Please input temperature:"
-Input1:40
-Output:It's too hot.
-"""
-
-t = float(input("溫度(℃ ):"))
-if t >= 40:
-    print("\n太熱")
-elif t <= 10:
-    print("\n太冷")
+word = input("請輸入密碼:")
+if word == "6666":
+    print("\n歡迎光臨")
+elif word=="8888":
+    print("\nYou are VIP")
 else:
-    print("\n舒適")
+    print("\n錯誤")
+print("\n程式結束")
