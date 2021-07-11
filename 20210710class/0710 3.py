25dd3c9a0cb6df543638e92b683875513499c466
jack chen24 <86872075+Weiting71924@users.noreply.github.com>
2021-07-11 08:27:33 +0000

    0711

--- 20210710class/0710 3.py
+++ 20210710class/0710 3.py
@@ -1,11 +1,11 @@
 word = input("請輸入密碼:")
 if word == "6666":
-    print("歡迎光臨")
+    print("\n歡迎光臨")
 elif word=="8888":
-    print("You are VIP")
+    print("\nYou are VIP")
 else:
-    print("錯誤")
-print("程式結束")
+    print("\n錯誤")
+print("\n程式結束")
 
 
 a = int(input("請輸入成績:"))
