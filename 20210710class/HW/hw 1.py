8e3ca848811efa95f3750f093e3f2a9ee3659320
jack chen24 <86872075+Weiting71924@users.noreply.github.com>
2021-07-11 02:43:55 +0000

    0711

--- 20210710class/HW/hw 1.py
+++ 20210710class/HW/hw 1.py
@@ -12,3 +12,10 @@ Input1:6
 Input2:9
 Output:False
 """
+a = float(input("請輸入分子:"))
+b = float(input("\n請輸入分母:"))
+if a / b == 7 / 9:
+    print("\nTrue")
+else:
+    print("\nFalse")
+print("\n程式結束")
