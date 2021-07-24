f8a71c8ea130d4986dd7894810037d212213b1ea
jack chen24 <86872075+Weiting71924@users.noreply.github.com>
2021-07-24 09:50:32 +0000

    0724

---
+++ 20210724class/0724.py
@@ -0,0 +1,25 @@
+juice_list = ["1", "2"]
+while True:
+    print("1  想加入菜單的果汁")
+    print("2  顯示菜單")
+    print("3  結束程式")
+    a = input("選擇步驟:")
+    if a == "1":
+        new = input("輸入果汁:")
+        if new in juice_list:
+          print("已有此果汁")
+        else:
+          juice_list.append(new)
+          print("新增完成")
+        # for l in juice_list:
+        #     if new == l:
+        #         print("已有此果汁")
+        #         break
+        # else:
+        #     juice_list.append(new)
+        #     print("新增完成")
+    if a == "2":
+        print(juice_list)
+    if a == "3":
+        print("結束程式")
+        break
