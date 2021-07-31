ae03db4de23522a50daa9a4f3afc201f01b36b53
jack chen24 <86872075+Weiting71924@users.noreply.github.com>
2021-07-31 05:09:12 +0000

    31

--- 20210727class/HW/hw.py
+++ 20210727class/HW/hw.py
@@ -4,15 +4,16 @@
 並使用function def 求最小公倍數
 value = lcm(num1, num2)
 '''
-
-# n1=input()
-# n2=input()
-
-
-
-
-
-
-
-
-
+def lcm(n1,n2):
+  if n1>n2:
+    b=n1
+  else:
+    b=n2
+  for a in range(b,n1*n2+1):
+    if a%n1==0 and a%n2==0:
+      lcm=a
+      break
+  return lcm
+num1=int(input('輸入一個數:'))
+num2=int(input('輸入一個數:'))
+print(lcm(num1, num2))
