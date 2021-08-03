f314023b1cca30665453033e55dad1b9cd4b205d
jack chen24 <86872075+Weiting71924@users.noreply.github.com>
2021-08-03 10:09:08 +0000

    8/3

--- 20210731class/hw2.py
+++ 20210731class/hw2.py
@@ -21,18 +21,18 @@ def fight(st):
     p_att=random.randint(1,3)
     get_money=random.randint(10,20)
     print('\n遇到怪物  HP:{}'.format(monsterHP))
-    print('戰鬥開始!!!!')
-    
+    print('!!!戰鬥開始!!!')
     for u in range(8):
-      time.sleep(1)
+      time.sleep(2)
       print('\n玩家攻擊，造成{}點傷害'.format(p_att))
       monsterHP-=p_att
       print('怪物剩餘HP:{}'.format(monsterHP))
       if monsterHP<=0:
-        print('\n擊敗怪物!!!')
+        print('\n!!!!擊敗怪物!!!!')
         st[2]+=get_money
         print('獲得${}'.format(get_money, st[2]))
         return st
+      time.sleep(2)
       print('\n怪物攻擊')
       print('玩家剩餘HP:{}'.format(st[1]))
       st[1]-=1
@@ -48,7 +48,7 @@ while True:
     case = random.randrange(0, len(func_list))
     status = func_list[case](status)
     print("玩家狀態 = {}".format(status))
-    time.sleep(1)
+    time.sleep(1.5)
     if status[0] == 0:
-        print("Game Over QAQ!!")
+        print("Game Over")
         break
